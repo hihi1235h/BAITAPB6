3a16ea6993aeb371d34f4899af1e0523660a4604
LÊ NHI <leyennhi170705@gmail.com>
2025-12-12 11:11:39 +0700

    Add files via upload

---
+++ Model1.cs
@@ -0,0 +1,22 @@
+using System;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Data.Entity;
+using System.Linq;
+
+namespace BTB6_01.Model
+{
+    public partial class Model1 : DbContext
+    {
+        public Model1()
+            : base("name=Model1")
+        {
+        }
+
+        public virtual DbSet<Faculty> Faculties { get; set; }
+        public virtual DbSet<Student> Students { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+        }
+    }
+}
